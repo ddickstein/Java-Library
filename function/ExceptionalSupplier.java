b5828676c87af97c451f29016d1a57a2ba34b5cd
ddickstein <danidickstein@yahoo.com>
2014-08-11 01:14:41 -0400

    Update ExceptionalSupplier.java

--- function/ExceptionalSupplier.java
+++ function/ExceptionalSupplier.java
@@ -1,6 +1,6 @@
 package library.function;
 
-// Idea for this class came from Alex Reinking
+// Idea for this interface came from Alex Reinking - allows support for anticipating checked exceptions
 
 public interface ExceptionalSupplier<T> {
   public T get() throws Exception;
